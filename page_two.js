37ba6b5ff22c340d67b4cd7dae36abe077ba41b1
NovGrad24 <qrentella.adams1@smail.rasmussen.edu>
2023-11-28 23:43:35 -0500

    with arrays

---
+++ page_two.js
@@ -0,0 +1,15 @@
+var headingtext= "<h2>Enter The Safe Space For Couch Potatoes!</h2>";
+var myintro= "Judgement Free Zone";
+linktag= "<a href=\"http://sotd.us/qrentellaadams1/pagetwo.html">Online Form</a>";
+
+/ The <span> tag is much like the <div> element, but <div> is a block-level element and <span> is an inline element:
+
+let red text= "<span style="color: #8B0000">Thank you for being here!</span>"
+let begineffect="<strong>";
+let endeffect= "</strong>";
+let beginpara= "<p>";
+let endpara= "</p>";
+
+document.write(myheading);
+document.write(begineffect + sometext + endeffect);
+document.write(beginpara + redtext + endpara);
---
+++ pagethree.js
@@ -0,0 +1,55 @@
+let text=("Please enter a number between 5 and 20.");
+let i= 5;
+
+do {
+  text += "<br> The number you entered is" + i;
+  i++;
+}
+
+while (i < 20);
+
+  document.getElementById("").innerHTML = text;
+
+var counter = 0;
+
+// function to for numbers of array
+
+function sortOddEven(array) {
+  const n = array.length;
+  let text;
+  do {
+    switch (let i = 0; i < 2) {
+      case expression:
+        text = "even numbers"
+        break;
+      default:
+        text = "Even you are great!"
+    }
+    document.getElementById("").innerHTML = text;
+
+    // sort the even numbers
+    for (let i = 0; i < 2) {
+      if (arr[i] > arr[i < 2]) {
+        [arr[i], arr[i < 2]];
+        switch (let i = 0; i < 2) {
+
+          break;
+        default:
+      switch= true;
+      }
+    }
+    // sort the odd numbers
+    for (let i = 1; i < 10){
+      if arr[i], arr [i < 10]];
+    switch (let i = 1; i < 10) {
+
+        break;
+      default:
+    switch= true;
+  }
+} while (true);
+return arr;
+
+}
+const array = [""];
+console.log(sortOddEven(arr));
